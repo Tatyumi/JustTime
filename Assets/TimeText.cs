83b1d478785b9b3bde1ddd748de55a7bf581c433
Tatyumi <s150204@nishitech.ac.jp>
2018-12-29 17:54:44 +0900

    First Commit

---
+++ Assets/TimeText.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
+
+public class TimeText : MonoBehaviour {
+    private new AudioSource audio;
+
+    public AudioClip startButtonSe;
+    public AudioClip stopButtonSe;
+    public AudioClip clockSe;
+    public AudioClip gameSet;
+    public AudioClip continuePanelSe;
+
+    private int targetTime;
+    int count = 0;
+
+    public float startTime;
+    public float pushTime;
+    public float stopTime1;
+    public float stopTime2;
+
+    private float differenceValueP1;
+    private float differenceValueP2;
+
+    public Text targetTimeText;
+    public Text targetTimeText_G;
+    public Text startTimeText;
+    public Text stopTime1Text;
+    public Text stopTime2Text;
+    public Text win1pText;
+    public Text win2pText;
+
+    public GameObject startPanel;
+    public GameObject countinuePanel;
+
+    private bool isTapP1;
+    private bool isTapP2;
+    private bool isCheck;
+    private bool isBit;
+    private bool isEndSe;
+
+    void Start () {
+        audio = gameObject.GetComponent<AudioSource>();
+        startPanel.SetActive(true);
+        countinuePanel.SetActive(false);
+        targetTime = UnityEngine.Random.Range(5,11);
+        targetTimeText.text = "TargetTime\n"+targetTime.ToString()+"s";
+        targetTimeText_G.text = targetTime.ToString()+"s";
+        startTime = 0;
+        stopTime1 = 0;
+        stopTime2 = 0;
+
+        stopTime1Text.enabled = false;
+        stopTime2Text.enabled = false;
+        startTimeText.enabled = false;
+        win1pText.enabled = false;
+        win2pText.enabled = false;
+        targetTimeText_G.enabled = false;
+
+        isTapP1 = false;
+        isTapP2 = false;
+        isCheck = true;
+        isEndSe = true;
+
+    }
+
+    void Update () {
+        //startTime += 1 * Time.deltaTime;
+        startTime = Time.time - pushTime;
+        startTimeText.text = startTime.ToString("f2");
+
+        if (targetTime / 2 < startTime)
+            startTimeText.enabled = false;
+
+        if (isTapP1 == true && isTapP2 == true && isCheck == true)
+        {
+            audio.Stop();
+            checkTime();
+        }
+        else if (isCheck == false)
+        {
+            count++;
+        }
+
+        if (count == 160)
+            if (isEndSe == true)
+            {
+                isEndSe = false;
+                audio.PlayOneShot(continuePanelSe);
+                countinuePanel.SetActive(true);
+            }
+    }
+    //audio.PlayOneShot(SceneMoveSe);
+
+    public void timeStop(int p)
+    {
+        audio.PlayOneShot(stopButtonSe);
+        if (p == 1 && isTapP1 == false)
+        {
+            stopTime1Text.enabled = true;
+            stopTime1 = Time.time - pushTime;
+            //stopTime1Text.text = stopTime1.ToString("f2");
+            stopTime1Text.text = "STOP!";
+            isTapP1 = true;
+
+        }
+        else if (p == 2 && isTapP2 == false)
+        {
+            stopTime2Text.enabled = true;
+            stopTime2 = Time.time - pushTime;
+            //stopTime2Text.text = stopTime2.ToString("f2");
+            stopTime2Text.text = "STOP!";
+            isTapP2 = true;
+        }
+    }
+
+    public void checkTime()
+    {
+        audio.PlayOneShot(gameSet);
+
+        stopTime1Text.text = stopTime1.ToString("f2");
+        stopTime2Text.text = stopTime2.ToString("f2");
+        Debug.Log("チェックタイム！");
+        differenceValueP1 = Math.Abs(targetTime - stopTime1);
+        differenceValueP2 = Math.Abs(targetTime - stopTime2);
+
+        if(differenceValueP1 < differenceValueP2)
+        {//1Pの勝利
+            Debug.Log("1pのかち！");
+            win1pText.enabled = true;
+        }
+        else if (differenceValueP1 > differenceValueP2)
+        {//2Pの勝利
+            Debug.Log("2pの勝ち");
+            win2pText.enabled = true;
+        }
+        else
+        {
+            //Draw!
+        }
+
+        startTimeText.enabled = false;
+        isCheck = false;
+        //終了，もしくは再開するパネルの表示
+    }
+
+    public void pushStartButton()
+    {
+        audio.PlayOneShot(startButtonSe);
+        audio.PlayOneShot(clockSe);
+
+        pushTime = Time.time;
+        targetTimeText_G.enabled = true;
+        startTimeText.enabled = true;
+        startPanel.SetActive(false);
+    }
+
+    public void countinueGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void gameEnd()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+}
