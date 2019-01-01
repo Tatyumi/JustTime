8638f61362387dd210c3970fcc8f195fa4ee1793
Tatyumi <s150204@nishitech.ac.jp>
2019-01-01 09:33:47 +0900

    コメント修正

--- Assets/TitleDirector.cs
+++ Assets/TitleDirector.cs
@@ -3,14 +3,11 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class TitleDirector : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-    }
-	
+    
 	// Update is called once per frame
 	void Update () {
 
+        //タップされた場合
         if (Input.GetMouseButtonDown(0)) { 
             SceneManager.LoadScene("GameScene");
         }
