37cd30f327eca2120400afdf2d932eec82a4b078
Tatyumi <s150204@nishitech.ac.jp>
2019-02-22 21:12:23 +0900

    新規追加
    共通して定数を扱えるようにするため，追加．

---
+++ Assets/Common.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Common
+{
+    // 定数クラス
+    public static class Constans
+    {
+        /// <summary>秒針の音声ファイル名</summary>
+        public const string CLOCK_SE = "ClockSE";
+
+        /// <summary>コンテニューパネル表示の音声ファイル名</summary>
+        public static string CONTINUE_PANEL_SE= "ContinuePanelSE";
+        /// <summary>ゲーム終了音の音声ファイル名</summary>
+        public static string GAME_SET_SE ="GameSetSE";
+        /// <summary>開始ボタンプッシュ音の音声ファイル名</summary>
+        public static string START_BUTTON_SE ="StartButtonSE";
+        /// <summary>停止ボタンプッシュ音の音声ファイル名</summary>
+        public static string STOP_BUTTON_SE ="StopButtonSE";
+    }
+}
