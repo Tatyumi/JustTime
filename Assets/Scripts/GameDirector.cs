6534b4390a9d6b373f80a63a902935ba73415c62
Tatyumi <s150204@nishitech.ac.jp>
2019-02-27 11:10:03 +0900

    コードを一部修正
    変数名や宣言順，メソッドの記載場所などを修正した．

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -7,25 +7,14 @@ using Common;
 
 public class GameDirector : MonoBehaviour
 {
-
-    /// <summary>指定目標時間</summary>
-    private int targetTime;
-    int count = 0;
-
     /// <summary>計測開始時間</summary>
     public float StartTime;
     /// <summary>シーン移行から開始ボタンを押すまでの時間</summary>
     public float PushTime;
     /// <summary>プレイヤー1の計測時間</summary>
-    public float StopTime1;
+    public float StopTimeP1;
     /// <summary>プレイヤー2の計測時間</summary>
-    public float StopTime2;
-
-    /// <summary>プレイヤー1の目標時間と計測時間の差の値</summary>
-    private float differenceValueP1;
-    /// <summary>プレイヤー2の目標時間と計測時間の差の値</summary>
-    private float differenceValueP2;
-
+    public float StopTimeP2;
     /// <summary>目標計測時間のテキスト</summary>
     public Text TargetTimeText;
     /// <summary>表示用目標計測時間のテキスト</summary>
@@ -40,12 +29,19 @@ public class GameDirector : MonoBehaviour
     public Text Win1pText;
     /// <summary>プレイヤー2の勝敗テキスト</summary>
     public Text Win2pText;
-
     /// <summary>ゲーム開始待機パネル</summary>
     public GameObject StartPanel;
     /// <summary>ゲームを続けるか促すパネル</summary>
     public GameObject CountinuePanel;
+    /// <summary>オーディオマネージャー</summary>
+    public GameObject AudioManager;
 
+    /// <summary>プレイヤー1の目標時間と計測時間の差の値</summary>
+    private float differenceValueP1;
+    /// <summary>プレイヤー2の目標時間と計測時間の差の値</summary>
+    private float differenceValueP2;
+    /// <summary>指定目標時間</summary>
+    private int targetTime;
     /// <summary>プレイヤー1がストップボタンを一度目に押したかを判別するフラグ</summary>
     private bool isTapP1;
     /// <summary>プレイヤー2がストップボタンを一度目に押したかを判別するフラグ</summary>
@@ -53,11 +49,11 @@ public class GameDirector : MonoBehaviour
     /// <summary></summary>
     private bool isCheck;
     /// <summary></summary>
-    private bool isEndSe;
-    /// <summary>オーディオマネージャー</summary>
-    public GameObject AudioManager;
+    private bool isEndSE;
     /// <summary></summary>
     private AudioManager audioManager;
+
+    int count = 0;
 
     private void Awake()
     {
@@ -72,8 +68,8 @@ public class GameDirector : MonoBehaviour
         TargetTimeText.text = "TargetTime\n" + targetTime.ToString() + "s";
         TargetTimeText_G.text = targetTime.ToString() + "s";
         StartTime = 0;
-        StopTime1 = 0;
-        StopTime2 = 0;
+        StopTimeP1 = 0;
+        StopTimeP2 = 0;
 
         StopTime1Text.enabled = false;
         StopTime2Text.enabled = false;
@@ -85,7 +81,7 @@ public class GameDirector : MonoBehaviour
         isTapP1 = false;
         isTapP2 = false;
         isCheck = true;
-        isEndSe = true;
+        isEndSE = true;
 
     }
 
@@ -97,14 +93,16 @@ public class GameDirector : MonoBehaviour
 
         //開始ボタンを押してから指定目標時間の半分に達したか判別
         if (targetTime / 2 < StartTime)
+        {
             //計測時間を非表示にする
             StartTimeText.enabled = false;
+        }
 
         //両プレイヤーがストップボタンを押したか判別
         if (isTapP1 == true && isTapP2 == true && isCheck == true)
         {
             audioManager.StopSE();
-            CheckTime();
+            Judge();
         }
         else if (isCheck == false)
         {
@@ -115,25 +113,39 @@ public class GameDirector : MonoBehaviour
         if (count == 160)
 
             //修了の効果音を流したか判別
-            if (isEndSe == true)
+            if (isEndSE == true)
             {
-                isEndSe = false;
+                isEndSE = false;
                 audioManager.PlaySE(Constans.CONTINUE_PANEL_SE);
                 CountinuePanel.SetActive(true);
             }
     }
 
     /// <summary>
+    /// 計測開始処理
+    /// </summary>
+    public void StartClock()
+    {
+        audioManager.PlaySE(Constans.START_BUTTON_SE);
+        audioManager.PlaySE(Constans.CLOCK_SE);
+
+        PushTime = Time.time;
+        TargetTimeText_G.enabled = true;
+        StartTimeText.enabled = true;
+        StartPanel.SetActive(false);
+    }
+
+    /// <summary>
     /// 計測終了処理
     /// </summary>
-    /// <param name="p"></param>
+    /// <param name="p">プレイヤー</param>
     public void StopClock(int p)
     {
         //ストップボタンを押したプレイヤーと初めに押したかを判別
         if (p == 1 && isTapP1 == false)
         {
             StopTime1Text.enabled = true;
-            StopTime1 = Time.time - PushTime;
+            StopTimeP1 = Time.time - PushTime;
             StopTime1Text.text = "STOP!";
             isTapP1 = true;
             audioManager.PlaySE(Constans.STOP_BUTTON_SE);
@@ -142,7 +154,7 @@ public class GameDirector : MonoBehaviour
         else if (p == 2 && isTapP2 == false)
         {
             StopTime2Text.enabled = true;
-            StopTime2 = Time.time - PushTime;
+            StopTimeP2 = Time.time - PushTime;
             StopTime2Text.text = "STOP!";
             isTapP2 = true;
             audioManager.PlaySE(Constans.STOP_BUTTON_SE);
@@ -152,15 +164,15 @@ public class GameDirector : MonoBehaviour
     /// <summary>
     /// 両プレイヤーの計測時間と指定目標時間を比較し，勝敗をつける
     /// </summary>
-    public void CheckTime()
+    public void Judge()
     {
         audioManager.PlaySE(Constans.GAME_SET_SE);
-        StopTime1Text.text = StopTime1.ToString("f2");
-        StopTime2Text.text = StopTime2.ToString("f2");
+        StopTime1Text.text = StopTimeP1.ToString("f2");
+        StopTime2Text.text = StopTimeP2.ToString("f2");
 
         // 目標時間と計測時間の差分を取得
-        differenceValueP1 = Math.Abs(targetTime - StopTime1);
-        differenceValueP2 = Math.Abs(targetTime - StopTime2);
+        differenceValueP1 = Math.Abs(targetTime - StopTimeP1);
+        differenceValueP2 = Math.Abs(targetTime - StopTimeP2);
 
         //どのプレイヤーが勝利したか判別
         if (differenceValueP1 < differenceValueP2)
@@ -180,20 +192,6 @@ public class GameDirector : MonoBehaviour
 
         StartTimeText.enabled = false;
         isCheck = false;
-    }
-
-    /// <summary>
-    /// 計測開始処理
-    /// </summary>
-    public void StartClock()
-    {
-        audioManager.PlaySE(Constans.START_BUTTON_SE);
-        audioManager.PlaySE(Constans.CLOCK_SE);
-
-        PushTime = Time.time;
-        TargetTimeText_G.enabled = true;
-        StartTimeText.enabled = true;
-        StartPanel.SetActive(false);
     }
 
     /// <summary>
